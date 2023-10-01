65183f49bb6476f8cc76e1f12df4128bf5a2efe0
2004Haiqa <144584367+2004Haiqa@users.noreply.github.com>
2023-10-01 19:14:04 +0500

    Add files via upload

---
+++ To Do List/Script3.js
@@ -0,0 +1,123 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+    // Load tasks from local storage when the page loads
+    loadTasks();
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const taskText = input.value;
+
+        if (!taskText) {
+            alert("Please fill out the task");
+            return;
+        }
+
+        const taskId = Date.now().toString(); // Generate a unique ID for the task
+
+        // Create a new task object
+        const task = {
+            id: taskId,
+            text: taskText
+        };
+
+        // Add the task to the DOM
+        addTaskToDOM(task);
+
+        // Update local storage with the new task
+        updateLocalStorage();
+
+        input.value = "";
+    });
+
+    function updateLocalStorage() {
+        const tasks = [];
+        const taskElements = document.querySelectorAll(".task");
+
+        taskElements.forEach(taskElement => {
+            const taskId = taskElement.getAttribute("data-id");
+            const taskText = taskElement.querySelector(".text").value;
+
+            tasks.push({ id: taskId, text: taskText });
+        });
+
+        // Store the task list in local storage
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    function loadTasks() {
+        const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+        if (savedTasks && savedTasks.length > 0) {
+            savedTasks.forEach(task => {
+                // Add each saved task to the DOM
+                addTaskToDOM(task);
+            });
+        }
+    }
+
+    function addTaskToDOM(task) {
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+        task_el.setAttribute("data-id", task.id);
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+
+        task_el.appendChild(task_content_el);
+
+        const task_input_el = document.createElement("input");
+        task_input_el.classList.add("text");
+        task_input_el.type = "text";
+        task_input_el.value = task.text;
+        task_content_el.appendChild(task_input_el);
+
+        const task_actions_el = document.createElement("div");
+        task_actions_el.classList.add("actions");
+
+        const task_edit_el = document.createElement("button");
+        task_edit_el.classList.add("edit");
+        task_edit_el.innerHTML = "Edit";
+
+        const task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerHTML = "Delete";
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el);
+
+        list_el.appendChild(task_el);
+
+        // Add event listeners for editing and deleting
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerText.toLowerCase() == "edit") {
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                task_edit_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText = "Edit";
+                task.text = task_input_el.value;
+                updateLocalStorage();
+            }
+        });
+
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+            // Remove the task from the local storage
+            removeTaskFromLocalStorage(task.id);
+        });
+    }
+
+    function removeTaskFromLocalStorage(taskId) {
+        const tasks = JSON.parse(localStorage.getItem("tasks"));
+        const updatedTasks = tasks.filter(task => task.id !== taskId);
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    }
+});
+
